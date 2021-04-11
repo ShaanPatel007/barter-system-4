20a5f2c9361fb0d01d0dfc3fc36485b3048dc835
ShaanPatel007 <67004131+ShaanPatel007@users.noreply.github.com>
2021-04-11 09:38:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import UserLoginAndSignup from './screens/UserLoginAndSignupScreen';
+
+export default function App() {
+  return (
+    <UserLoginAndSignup/>
+  );
+}
